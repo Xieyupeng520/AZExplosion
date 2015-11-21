e89976ffa6ccd5dd097d3f576009bbfa29ca7f70
Xieyupeng520 <806622085@qq.com>
2015-11-21 11:26:44 +0800

    新建layout展示最终效果（activity_main_az）。

--- app/src/main/java/explosionfield/azz/com/azexplosionfield/MainActivity.java
+++ app/src/main/java/explosionfield/azz/com/azexplosionfield/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_1);
+        setContentView(R.layout.activity_main_az);
 
         ExplosionField explosionField = new ExplosionField(this);
 
